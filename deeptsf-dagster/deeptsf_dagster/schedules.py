31fbf3c6fd3a60c83f82d937da2d97c213eacf80
TzorAL <tzortzisalex99@gmail.com>
2024-12-09 16:04:49 +0200

    Bug fix: scheduled datetime did not pass to config resource

--- deeptsf-dagster/deeptsf_dagster/schedules.py
+++ deeptsf-dagster/deeptsf_dagster/schedules.py
@@ -7,12 +7,16 @@ from dagster import (
 import os
 from .jobs import deeptsf_dagster_job
 from .config import DeepTSFConfig
+import pytz
 
 # we do this to change the current date at each run it scedules 
-@schedule(job=deeptsf_dagster_job, cron_schedule="0 * * * *", execution_timezone='Europe/Athens') # on Monday at 00:00 GMT+3
+@schedule(job=deeptsf_dagster_job, cron_schedule="0 * * * *", execution_timezone='Europe/Athens') # on Monday at 00:00 GMT+2
 def deeptsf_dagster_schedule(context: ScheduleEvaluationContext):
-
+    # Ensure the scheduled execution time is in the Europe/Athens timezone
+    # athens_tz = pytz.timezone('Europe/Athens')
+    # scheduled_date = context.scheduled_execution_time.astimezone(athens_tz).strftime('%Y-%m-%d %H:%M:%S')
     scheduled_date = context.scheduled_execution_time.strftime('%Y-%m-%d %H:%M:%S')
+
     config = DeepTSFConfig().to_dict()
     config['forecast_start'] = scheduled_date
 
@@ -21,7 +25,7 @@ def deeptsf_dagster_schedule(context: ScheduleEvaluationContext):
         run_config={
             "resources": {
                 "config": {
-                    "config": DeepTSFConfig().to_dict()
+                    "config": config
                 },
             },
         },
